466d503e3117fa30d3dc77bf5be2d710fa5245ee
PouyaGohari <pouya.hmgohari@gmail.com>
2023-09-01 20:47:21 +0330

    Inverse & reverse functions added in order to create thewanted string

--- Question1.cpp
+++ Question1.cpp
@@ -44,8 +44,50 @@ my_input assign_to_my_input(const vector<string> lines){
     return result;
 } 
 
+void inverse_fnc(string& str, int it = 0){
+    if(it == str.size() - 1){
+        if(str[it] == '0') str[it] = '1';
+        else str[it] = '0';
+        return;
+    }
+    if(str[it] == '0') str[it] = '1';
+    else str[it] = '0';
+    it = it + 1;
+    inverse_fnc(str, it);
+}
+
+void reverse_fnc(string& str, int it = 0){
+    if(it == str.size()/2 && str.size()%2 == 1){
+        swap(str[it], str[str.size()- 1 - it]);
+        return; 
+    }
+    else if(it == str.size()/2 && str.size()%2 == 0){
+        return;
+    }
+    swap(str[it], str[str.size() - 1 - it]);
+    it = it + 1;
+    reverse_fnc(str, it);
+}
+
+string create_sn_recursively(int n){
+    if(n == 1){
+        return "0";
+    }
+    string previous_sequence = create_sn_recursively(n - 1);
+    string inv_rev_prev_seq = previous_sequence;
+    inverse_fnc(inv_rev_prev_seq);
+    reverse_fnc(inv_rev_prev_seq);
+    return previous_sequence + "1" + inv_rev_prev_seq;
+}
+
+void finding(int n, int k){
+    string seq = create_sn_recursively(n);
+    cout << seq[k-1] << endl;
+}
+
 int main(int arg , char*argv[]){
     my_input inputs = assign_to_my_input(reading_from_file(argv[1]));
     exception_handeling(inputs);
+    finding(inputs.n, inputs.k); 
     return 0;
 }

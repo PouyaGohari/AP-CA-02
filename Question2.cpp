aca9745f0b8c7dace9000221be11f69d4010bf55
PouyaGohari <pouya.hmgohari@gmail.com>
2023-09-03 19:12:35 +0330

    Reading from input file function has been added

--- Question2.cpp
+++ Question2.cpp
@@ -5,7 +5,14 @@
 
 using namespace std;
 
+string reading_from_file(const string file_name){
+    ifstream input_file(file_name);
+    string line;
+    getline(input_file, line);
+    return line;
+}
+
 int main(int argc, char*argv[]){
-    
+    string line = reading_from_file(argv[1]);
     return 0;
 }

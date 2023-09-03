5dea78394b3344d24be23a1a27e863087b544761
PouyaGohari <pouya.hmgohari@gmail.com>
2023-09-03 19:19:47 +0330

    Exception handeling added in order input file does not being ignored by limits

--- Question2.cpp
+++ Question2.cpp
@@ -5,10 +5,19 @@
 
 using namespace std;
 
+const int MIN_LENGTH = 1;
+const int MAX_LENGHT = 30;
+
+void exception_handeling(const string line){
+    if(line.length() > MAX_LENGHT || line.length() < MIN_LENGTH)
+     throw runtime_error("Limits for length has been ignored ur input length must be more than 0 and less than 31");
+}   
+
 string reading_from_file(const string file_name){
     ifstream input_file(file_name);
     string line;
     getline(input_file, line);
+    exception_handeling(line);
     return line;
 }
 

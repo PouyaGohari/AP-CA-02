3e08f672507115e210ff6eae0f497d31243bc9ab
PouyaGohari <pouya.hmgohari@gmail.com>
2023-09-08 12:41:31 +0330

    Define a necessary struct and reshape the input to this struct to have more efficient memory and time management

--- Question3.cpp
+++ Question3.cpp
@@ -15,6 +15,11 @@ struct given_inputs{
      meaning_words(mean_val), sentences(sent_val) {}
 };
 
+struct fitted_word_per_sentence{
+    vector<string> suitable_words;
+    string sentece;
+};
+
 vector<string> reading_from_file(const string file_name){
     ifstream input_file(file_name);
     string line;
@@ -44,7 +49,29 @@ given_inputs filling_strcut_with_inputs(const vector<string> lines){
     return result;
 }
 
+vector<string> finding_suitable_words(const string sentence, const vector<string> meaning_words){
+    vector<string> suitable_words;
+    for(auto word : meaning_words){
+        size_t found = sentence.find(word);
+        if(found == string::npos){
+            suitable_words.push_back(word);
+        }
+    }
+    return suitable_words;
+}
+
+vector<fitted_word_per_sentence> reassigning(const given_inputs inputs){
+    vector<fitted_word_per_sentence> result;
+    for(auto sentence : inputs.sentences){
+        fitted_word_per_sentence temp;
+        temp.suitable_words = finding_suitable_words(sentence, inputs.meaning_words);
+        temp.sentece = sentence;
+        result.push_back(temp);
+    }
+    return result;
+}
+
 int main(int argc, char*argv[]){
-    given_inputs my_inputs = filling_strcut_with_inputs(reading_from_file(argv[1]));
+    vector<fitted_word_per_sentence> inputs = reassigning(filling_strcut_with_inputs(reading_from_file(argv[1])));
     return 0;
 }
